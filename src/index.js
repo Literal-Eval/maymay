f03ab0d6fd00ce79ffae0a4bd2960cca865a9194
Ravi Dev Pandey <pandeyravidev@gmail.com>
2022-02-04 11:05:02 +0530

    fix title

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Home from "./components/Home";
+import Home from "./components/home/Home";
 
 ReactDOM.render(React.createElement(Home), document.querySelector('.root'));
